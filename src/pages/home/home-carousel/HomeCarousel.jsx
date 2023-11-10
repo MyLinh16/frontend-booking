b7fd0210bbceeb92bbd5aa33ebbdb3cbb47368eb
MyLinh16 <92028341+MyLinh16@users.noreply.github.com>
2023-11-11 01:51:39 +0700

    update

---
+++ src/pages/home/home-carousel/HomeCarousel.jsx
@@ -0,0 +1,32 @@
+import { Button, Carousel, Typography } from 'antd';
+import { carouselConfigs } from './carousel.configs';
+import styles from './styles.module.css';
+
+const HomeCarousel = () => {
+  return (
+    <div className={styles['carousel-container']}>
+      <Carousel autoplay>
+        {carouselConfigs.map((config, idx) => (
+          <div key={`carousel-${idx}`} className='position-relative'>
+            <div className='center-position'>
+              <Typography.Title
+                level={1}
+                className='text-center text-nowrap'
+                style={{ color: '#fff' }}>
+                {config.sologan}
+              </Typography.Title>
+            </div>
+            <img src={config.img} alt={config.sologan} />
+          </div>
+        ))}
+      </Carousel>
+      <div className='center-position mt-5'>
+        <Button size='large'>
+          <a href='#homeServices'>Khám phá ngay</a>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCarousel;
---
+++ src/pages/home/home.configs.js
@@ -0,0 +1,9 @@
+export const homeServiceConfigs = [
+  {
+    title: 'Đặt Tour Du Lịch',
+    img: 'https://i.pinimg.com/736x/72/e2/ce/72e2ce79110644e6c37d9ccb31c648ad.jpg',
+    path: '/tours',
+  },
+];
+
+export const homeFavoritePlaceConfigs = [];
